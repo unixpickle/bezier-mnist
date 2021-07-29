80167f18460f110153157b9c6c7cb3540f7db9c4
Alex Nichol <unixpickle@gmail.com>
2021-07-29 09:18:27 -0400

    basic code to create dataset

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/unixpickle/bezier-mnist
+
+go 1.16
+
+require (
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/unixpickle/anydiff v0.0.0-20170906235721-55bea27bf48b // indirect
+	github.com/unixpickle/anynet v0.0.0-20170909172929-016782221a5a // indirect
+	github.com/unixpickle/anyvec v0.0.0-20170908190750-59aa66ba0472 // indirect
+	github.com/unixpickle/autofunc v0.0.0-20170112172612-f27a3f82164a // indirect
+	github.com/unixpickle/essentials v1.3.0
+	github.com/unixpickle/mnist v0.0.0-20170128023510-751c4271cf3a
+	github.com/unixpickle/model3d v0.2.15
+	github.com/unixpickle/num-analysis v0.0.0-20161229165253-c45203c63047 // indirect
+	github.com/unixpickle/serializer v0.0.0-20170723202158-c6c092dc55bb // indirect
+	github.com/unixpickle/sgd v0.0.0-20161225162810-0e3d4c9d317b // indirect
+	github.com/unixpickle/tensor v0.0.0-20170114180418-7295881ed12b // indirect
+	github.com/unixpickle/weakai v0.0.0-20170623211141-247102c87396 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/mnist"
+	"github.com/unixpickle/model3d/model2d"
+)
+
+func main() {
+	for _, name := range []string{"train", "test"} {
+		var dataset mnist.DataSet
+		if name == "train" {
+			dataset = mnist.LoadTrainingDataSet()
+		} else {
+			dataset = mnist.LoadTestingDataSet()
+		}
+		os.Mkdir(name, 0755)
+		essentials.ConcurrentMap(0, len(dataset.Samples), func(i int) {
+			sample := dataset.Samples[i]
+			name := fmt.Sprintf("%s/%05d", name, i)
+			mesh := SampleToMesh(sample)
+			beziers := MeshToBeziers(mesh)
+			obj := map[string]interface{}{
+				"label":   sample.Label,
+				"beziers": beziers,
+			}
+			data, err := json.Marshal(obj)
+			essentials.Must(err)
+			essentials.Must(ioutil.WriteFile(name+".json", data, 0644))
+		})
+	}
+}
+
+func MeshToBeziers(m *model2d.Mesh) [][]model2d.BezierCurve {
+	fitter := &model2d.BezierFitter{
+		Tolerance: 1e-5,
+		L2Penalty: 1e-8,
+		Momentum:  0.5,
+	}
+	curves := fitter.Fit(m)
+	var res [][]model2d.BezierCurve
+	for len(curves) > 0 {
+		subset := []model2d.BezierCurve{curves[0]}
+		for i, c := range curves[1:] {
+			subset = append(subset, c)
+			if c[3] == curves[0][0] {
+				curves = curves[i+2:]
+				break
+			}
+		}
+		res = append(res, subset)
+	}
+	return res
+}
+
+func SampleToMesh(sample mnist.Sample) *model2d.Mesh {
+	bmp := model2d.NewBitmap(28, 28)
+	for i, x := range sample.Intensities {
+		if x > 0.5 {
+			bmp.Data[i] = true
+		}
+	}
+	mesh := bmp.Mesh()
+	for _, iters := range []int{5, 4, 3, 2, 1, 0} {
+		m := mesh.SmoothSq(iters)
+		if m.Manifold() {
+			mesh = m
+			break
+		}
+	}
+	mesh = mesh.Subdivide(1)
+	for _, iters := range []int{20, 10, 5, 2, 1, 0} {
+		m := mesh.SmoothSq(iters)
+		if m.Manifold() {
+			return m
+		}
+	}
+	return mesh
+}
